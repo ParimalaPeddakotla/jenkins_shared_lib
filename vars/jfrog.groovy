4dafc9fdd5d029cf496b8c376af54202cea25e64
ParimalaPeddakotla <93815304+ParimalaPeddakotla@users.noreply.github.com>
2023-07-06 13:09:18 +0530

    Update jfrog.groovy

--- vars/jfrog.groovy
+++ vars/jfrog.groovy
@@ -1,9 +1,9 @@
-def server = Artifactory.server '44.203.146.99';
+def server = Artifactory.server '3.88.49.112';
 
 def uploadSpec = """{
                 "files": [{
                     "pattern": "home/ubuntu/Java_app_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar",
-                    "target": "ui/repos/tree/General/example-repo-local"
+                    "target": "example-repo-local"
                     }
                     ]
             }"""
